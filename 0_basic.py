3ee76983f253c0bf9b01250312f79253536a0757
Icerawe <icerawe@live.com>
2023-11-27 09:36:42 +0700

    update

--- 0_basic.py
+++ 0_basic.py
@@ -1,23 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = [
-    [[255, 40, 12], [0, 255, 39]],
-    [[0, 90, 255], [255, 225, 0]]
-]
+# Define the image array
+x = []
+x = np.array(x, dtype=np.uint8)  # Correct dtype
 
-x_gray = []
-for i in x:
-    avg = []
-    for j in i:
-        avg.append(np.mean(j))
-    x_gray.append(avg)
-
-x = np.array(x, dtype=np.uint)
-plt.imshow(x, cmap="gray")
-plt.show()
-
-
-x_gray = np.array(x_gray, dtype=np.uint)
-plt.imshow(x_gray, cmap="gray")
+# Display the image using matplotlib
+plt.imshow(x, cmap="gray")  # Corrected argument name
 plt.show()
